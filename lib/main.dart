b21abc4be336f8b580cb5c5f0728c6a41177ebbd
Fluke <jesadakorn.kirtnu@gmail.com>
2021-04-17 15:48:12 +0700

    add drawer and bottom up panel

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:location/location.dart';
-import 'package:sprout_partner/utils/Location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(UseStateExample());
 
@@ -15,11 +16,25 @@ class UseStateExample extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final index = useState(0);
+    final getOrder = useState(false);
     final Location _location = Location();
+    final radius = BorderRadius.only(
+      topLeft: Radius.circular(24.0),
+      topRight: Radius.circular(24.0),
+    );
+    final boxNav = SvgPicture.asset(
+      'assets/images/remove.svg',
+      color: Colors.grey[400],
+      semanticsLabel: 'A red up arrow',
+      height: 50,
+    );
+
     GoogleMapController _controller;
 
     useEffect(() {
-      _location.onLocationChanged.listen((newLocalData) {
+      _location.onLocationChanged.listen((localData) {
+        var newLocalData = localData;
+
         if (_controller != null) {
           _controller.animateCamera(CameraUpdate.newCameraPosition(
               new CameraPosition(
@@ -33,31 +48,104 @@ class UseStateExample extends HookWidget {
     }, const []);
 
     void _onItemTapped(int myIndex) {
-      print(myIndex);
       index.value = myIndex;
     }
 
     return MaterialApp(
         home: SafeArea(
       child: Scaffold(
-        body: Stack(
-          children: [
-            GoogleMap(
-              mapType: MapType.normal,
-              initialCameraPosition: camera,
-              buildingsEnabled: false,
-              trafficEnabled: true,
-              onMapCreated: (GoogleMapController controller) {
-                _controller = controller;
-              },
-              onCameraMove: (position) => print(position),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: Icon(Icons.person_pin_circle, size: 40.0),
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.green[500],
+          actions: [
+            Row(
+              children: [
+                Text('รับงาน'),
+                Switch(
+                    value: getOrder.value,
+                    onChanged: (bool newVal) {
+                      getOrder.value = newVal;
+                    })
+              ],
+            )
           ],
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('User'),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                ),
+              ),
+              ListTile(
+                title: Text('History'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Sprout Wallet'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Notification'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: SlidingUpPanel(
+            borderRadius: radius,
+            backdropEnabled: true,
+            collapsed: Container(
+              decoration: BoxDecoration(borderRadius: radius),
+              child: Column(
+                children: [
+                  boxNav,
+                ],
+              ),
+            ),
+            panel: Center(
+              child: Column(
+                children: [
+                  boxNav,
+                  Column(
+                    children: [
+                      Row(
+                        children: [Text('Credit')],
+                      ),
+                      ElevatedButton(
+                        child: Text('รับงาน'),
+                        onPressed: () => {},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            body: Stack(
+              children: [
+                GoogleMap(
+                  mapType: MapType.normal,
+                  initialCameraPosition: camera,
+                  buildingsEnabled: false,
+                  trafficEnabled: true,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller = controller;
+                  },
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Icon(Icons.person_pin_circle, size: 40.0),
+                ),
+              ],
+            )),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
